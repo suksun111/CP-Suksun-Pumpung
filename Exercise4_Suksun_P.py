746d10c7292451cf9c4d8b8174721583b96a265d
suksun111 <51019055+suksun111@users.noreply.github.com>
2019-06-10 22:05:42 +0700

    Create Exercise4_Suksun_P.py
    add_file Exercise4

---
+++ Exercise4_Suksun_P.py
@@ -0,0 +1,9 @@
+point_FE = 2.50
+point_GB = 5.60
+point_ICS = 20.00
+point_CP = 10.5
+print("--- Your Score ---")
+print("Foundation English :",point_FE)
+print("General Business :",point_GB)
+print("Introduction to Computer Systems :",point_ICS)
+print("Computer Programming :",point_CP)
